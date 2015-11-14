2e512040924d0afd7940c63070f6b7b4770bca4b
Jonathan AhNee <jonahnee@gmail.com>
2015-11-14 12:06:35 -1000

    work in progress

--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -4,6 +4,7 @@ var should = chai.should();
 var expect = chai.expect;
 
 var generator = require('../js/generator.js');
+var newArray = require('../js/generator.js');
 
 describe('Generator', function () {
   it('should be a function', function () {
@@ -11,24 +12,29 @@ describe('Generator', function () {
     expect(generator).to.be.a('function');
   });
 
-  it('must accept a number', function () {
+  it('should only accept a number', function () {
     expect(generator()).to.deep.equal([], 'should have a parameter');
     expect(generator(3)).to.not.deep.equal([], 'parameter should be a number');
+    // expect(generator()).to.be.a('number');
   });
 
   describe('the result', function() {   
 
     it('should return an array', function () {
-      expect(generator).to.be.a('array');
+      expect(generator()).to.be.a('array');
     });
     
+    it('the length should equal the number passed in', function () {
+      expect(generator(5).length).to.equal(5);
+    });
+
+    it('the numbers should not exceed the length value');
+
     it('should be in a random order');
     
     it('should be unique');
     
-    it('the numbers should not exceed the length value');
     
-    it('the length should equal the number passed in');
   
   });
 
