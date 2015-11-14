618f82056f8b3165d7bfbc9705859f8509510306
Jonathan AhNee <jonahnee@gmail.com>
2015-11-14 12:06:23 -1000

    work in progres

--- js/generator.js
+++ js/generator.js
@@ -1,7 +1,14 @@
 module.exports = generator;
 
 function generator (arrayLength) {
+  var newArray = [];
+
   if (!arrayLength) {  
     return [];
+  } else {
+    for(var i = 0; i < arrayLength; i++) {
+      newArray.push(i);
+    }
   }
+  return newArray;
 }
