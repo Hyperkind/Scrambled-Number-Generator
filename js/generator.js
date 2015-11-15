d292e1bdac0b3de93344910f6291c9da4c90e283
Jonathan AhNee <jonahnee@gmail.com>
2015-11-14 15:43:04 -1000

    it works

--- js/generator.js
+++ js/generator.js
@@ -2,6 +2,8 @@ module.exports = generator;
 
 function generator (arrayLength) {
   var newArray = [];
+  var ranNum = [];
+  var ranArray = [];
 
   if (!arrayLength) {  
     return [];
@@ -9,6 +11,18 @@ function generator (arrayLength) {
     for(var i = 0; i < arrayLength; i++) {
       newArray.push(i);
     }
+    // newArray.sort(function() {
+    //   return 0.5 - Math.random();
+    // });
+    while(newArray.length > 0) {
+      // (var j = 0; j < newArray; j++) {
+      var splicedNum = Math.floor(Math.random() * newArray.length);
+      ranNum.push(newArray.splice(splicedNum, 1)[0]);
+    }
   }
-  return newArray;
-}
+  console.log(ranNum);
+  return ranNum;
+}  
+
+
+generator(5);
